3297ea39a7b23b39cf000bb70f6c20ba7696f0c8
rsoyding <rodolphe.soyding@leclanche.com>
2024-04-14 20:51:03 +0200

    resolve import error

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-import caroa04
+import src.caroa04.caroa04
 
 # -- General configuration ---------------------------------------------
 
